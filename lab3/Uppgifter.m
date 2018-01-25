9c7867e0e8caa5f468a2f393ddd67c060553d823
Anton Lööf <loofa@net.chalmers.se>
2018-01-25 14:28:18 +0100

    ost!?

--- lab3/Uppgifter.m
+++ lab3/Uppgifter.m
@@ -16,8 +16,8 @@ axis([-1 2 -1 2]), grid on, axis equal;
 %% uppg 2 och 3
 clc, close all, clear all
 verts = [2*sqrt(2)/3 -sqrt(2)/3 -sqrt(2)/3 0;
-         0 sqrt(2/3) -sqrt(2/3) 0;
-         -1/3 -1/3 -1/3 1];
+         0            sqrt(2/3) -sqrt(2/3) 0;
+         -1/3        -1/3       -1/3       1];
 inds = [1 2 4;
         1 4 3;
         3 2 4;
